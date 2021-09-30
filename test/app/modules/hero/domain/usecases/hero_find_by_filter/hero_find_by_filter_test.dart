8aa268ed5bfb25c2b77139af779d9bc4b7cf9226
Vitor Hugo Américo <vitor_americo@hotmail.com>
2021-09-30 13:19:36 -0300

    implementado test no repository

--- test/app/modules/hero/domain/usecases/hero_find_by_filter/hero_find_by_filter_test.dart
+++ test/app/modules/hero/domain/usecases/hero_find_by_filter/hero_find_by_filter_test.dart
@@ -1,7 +1,41 @@
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:heroes/app/modules/hero/domain/repositories/i_hero_repository.dart';
+import 'package:heroes/app/modules/hero/domain/usecases/hero_find_by_filter/hero_find_by_filter.dart';
+import 'package:heroes/app/modules/hero/domain/usecases/hero_find_by_filter/i_hero_find_by_filter.dart';
+import 'package:heroes/app/modules/hero/infra/repositories/hero_repository.dart';
+import 'package:heroes/app/modules/hero/presenter/hero_module.dart';
+import 'package:mockito/annotations.dart';
+import 'package:modular_test/modular_test.dart';
 
+import 'hero_find_by_filter_test.mocks.dart';
+
+@GenerateMocks([HeroRepository])
 void main() {
-  testWidgets('hero find by filter ...', (tester) async {
-    // TODO: Implement test
+  setUpAll(() {
+    initModules([
+      HeroModule()
+    ], replaceBinds: [
+      Bind.singleton<IHeroRepository>((i) => MockHeroRepository()),
+    ]);
   });
+
+  test('deve estar com todas as instancias injetadas', () {
+    final repository = Modular.get<IHeroRepository>();
+    final heroFindByFilter = Modular.get<IHeroFindByFilter>();
+
+    expect(repository, isA<MockHeroRepository>());
+    expect(heroFindByFilter, isA<HeroFindByFilter>());
+  });
+
+  // test('deve retornar uma lista com os resultados', () async {
+  //   final repository = Modular.get<MockHeroRepository>();
+  //   final heroFindByFilter = Modular.get<IHeroFindByFilter>();
+
+  //   when(repository.findByName(any)).thenAnswer((_) => Future.value(List.empty()));
+
+  //   var result = await heroFindByFilter('batman');
+
+  //   expect(result, isA<List<HeroEntity>>());
+  // });
 }
--- test/app/modules/hero/infra/repositories/hero_repository_test.dart
+++ test/app/modules/hero/infra/repositories/hero_repository_test.dart
@@ -1,7 +1,45 @@
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:heroes/app/modules/hero/domain/errors/errors.dart';
+import 'package:heroes/app/modules/hero/domain/repositories/i_hero_repository.dart';
+import 'package:heroes/app/modules/hero/external/datasouces/hero_datasource.dart';
+import 'package:heroes/app/modules/hero/infra/datasources/i_hero_datasource.dart';
+import 'package:heroes/app/modules/hero/infra/repositories/hero_repository.dart';
+import 'package:heroes/app/modules/hero/presenter/hero_module.dart';
+import 'package:heroes/app/shared/connect/errors/http_response_error.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:modular_test/modular_test.dart';
 
+import 'hero_repository_test.mocks.dart';
+
+@GenerateMocks([HeroDatasource])
 void main() {
-  testWidgets('hero repository ...', (tester) async {
-    // TODO: Implement test
+  setUpAll(() {
+    initModules([
+      HeroModule()
+    ], replaceBinds: [
+      Bind.singleton<IHeroDatasource>((i) => MockHeroDatasource()),
+    ]);
+  });
+
+  test('deve estar com todas as instancias injetadas', () {
+    final datasource = Modular.get<IHeroDatasource>();
+    final repository = Modular.get<IHeroRepository>();
+
+    expect(datasource, isA<MockHeroDatasource>());
+    expect(repository, isA<HeroRepository>());
+  });
+
+  test('deve lançar um erro do tipo DatasourceError', () async {
+    final datasource = Modular.get<MockHeroDatasource>();
+    final repository = Modular.get<IHeroRepository>();
+
+    when(datasource.findByName(any)).thenThrow(HttpResponseError(
+      body: 'Erro desconhecido',
+      statusError: 'Erro interno do servidor',
+    ));
+
+    expect(() async => await repository.findByName(''), throwsA(isA<DatasourceError>()));
   });
 }
